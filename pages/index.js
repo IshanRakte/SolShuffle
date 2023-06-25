288a7063e52dd4f8ec49696668eda989f17e8556
Ishan <ishan.rakte@gmail.com>
2023-06-25 06:53:03 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { useEffect, useMemo, useState } from "react";
+
+
+import Main from "./Main";
+import { RPC_ENDPOINT } from "../utils";
+
+export default function Home() {
+
+  return (
+    <Main />
+  );
+}
